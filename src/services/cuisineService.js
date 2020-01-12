f8e769b8542a2ed9762ba9e02192aee1b7e0ef69
edsonha <edsonha@gmail.com>
2020-01-12 22:10:58 +0800

    Setup intial project

---
+++ src/services/cuisineService.js
@@ -0,0 +1,22 @@
+const cuisines = [
+  {
+    _id: "5c3430ecfc13ae122d000000",
+    name: "Western"
+  },
+  {
+    _id: "5c3430ecfc13ae122d000001",
+    name: "Japanese"
+  },
+  {
+    _id: "5c3430ecfc13ae122d000002",
+    name: "Thai"
+  },
+  {
+    _id: "5c3430ecfc13ae122d000003",
+    name: "Chinese"
+  }
+];
+
+export function getCuisines() {
+  return cuisines;
+}
