e1f2be2ce68266e23447ca6830268bdf064b802d
edsonha <edsonha@gmail.com>
2020-01-14 16:28:41 +0800

    Add test for sortBySelect functionality

--- src/components/HomePage/HomePage.test.js
+++ src/components/HomePage/HomePage.test.js
@@ -6,12 +6,13 @@ import * as RestaurantService from "../../services/restaurantService";
 const sampleData = [
   {
     _id: "5c342ac9fc13ae39f8000000",
-    name: "The Burger Bar by Fatboy's Concepts (Boat Quay)",
+    name: "Burger Bar by Fatboy's Concepts (Boat Quay)",
     address: "35 Boat Quay, 049824 Singapore",
     openingTime: "11:00 AM",
     closingTime: "10:30 PM",
     cuisine: { _id: "5c3430ecfc13ae122d000000", name: "Western" },
-    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000000.jpg"
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000000.jpg",
+    averagePrice: 25
   },
   {
     _id: "5c342ac9fc13ae39f8000003",
@@ -21,7 +22,8 @@ const sampleData = [
     openingTime: "11:00",
     closingTime: "22:00",
     cuisine: { _id: "5c3430ecfc13ae122d000001", name: "Japanese" },
-    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000003.jpg"
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000003.jpg",
+    averagePrice: 20
   }
 ];
 
@@ -51,7 +53,7 @@ describe("HomePage", () => {
     expect(getAllByText("Order").length).toEqual(2);
 
     fireEvent.click(filterBtnWestern);
-    expect(getByText(/The Burger Bar by Fatboy/i)).toBeInTheDocument();
+    expect(getByText(/Burger Bar by Fatboy/i)).toBeInTheDocument();
     expect(queryByText("Ramen Champion")).not.toBeInTheDocument();
     expect(getAllByText("Order").length).toEqual(1);
   });
@@ -65,8 +67,33 @@ describe("HomePage", () => {
     expect(getAllByText("Order").length).toEqual(1);
 
     fireEvent.click(filterBtnAll);
-    expect(getByText(/The Burger Bar by Fatboy/i)).toBeInTheDocument();
+    expect(getByText(/Burger Bar by Fatboy/i)).toBeInTheDocument();
     expect(getByText(/Ramen Champion/i)).toBeInTheDocument();
     expect(getAllByText("Order").length).toEqual(2);
+  });
+
+  it("should render restaurants that are sorted by name when the page is first loaded", () => {
+    const { getAllByTestId } = render(<HomePage />);
+
+    expect(getAllByTestId("restaurant-card-title").length).toEqual(2);
+    const first = getAllByTestId("restaurant-card-title")[0];
+    const second = getAllByTestId("restaurant-card-title")[1];
+
+    expect(first).toHaveTextContent(/Burger Bar by Fatboy/i);
+    expect(second).toHaveTextContent(/Ramen Champion/i);
+  });
+
+  it("should render restaurants that are sorted by price in ascending order when 'Sort By Average Price' is selected", () => {
+    const { getByTestId, getAllByTestId } = render(<HomePage />);
+    const select = getByTestId("sort-by-select");
+
+    fireEvent.change(select, { target: { value: "averagePrice" } });
+
+    const first = getAllByTestId("restaurant-card-title")[0];
+    const second = getAllByTestId("restaurant-card-title")[1];
+
+    expect(getAllByTestId("restaurant-card-title").length).toEqual(2);
+    expect(first).toHaveTextContent(/Ramen Champion/i);
+    expect(second).toHaveTextContent(/Burger Bar by Fatboy/i);
   });
 });
