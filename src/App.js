2a0415f868b4f79530a7ab01511f6c265a30a167
edsonha <edsonha@gmail.com>
2020-01-14 17:44:45 +0800

    Add react router page: homepage, adminpage and orderpage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./components/HomePage/HomePage";
+import OrderPage from "./components/OrderPage/OrderPage";
+import AdminPage from "./components/AdminPage/AdminPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/orders" component={OrderPage} />
+          <Route path="/admin" component={AdminPage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
