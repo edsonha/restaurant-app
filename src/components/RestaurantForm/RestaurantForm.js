60f5690161936e4b4dbc1f2dfd7f49e65a0c2916
edsonha <edsonha@gmail.com>
2020-01-15 14:52:06 +0800

    Change the cuisine input to select input and extract them into separate component

--- src/components/RestaurantForm/RestaurantForm.js
+++ src/components/RestaurantForm/RestaurantForm.js
@@ -1,53 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
+import { getCuisines } from "../../services/cuisineService";
+import SelectInput from "../common/Input/SelectInput";
 
-function RestaurantForm(props) {
-  const handleClick = () => {
-    props.history.replace(props.returnPath);
+class RestaurantForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cuisines: getCuisines()
+    };
+  }
+
+  handleClick = () => {
+    this.props.history.replace(this.props.returnPath);
   };
 
-  return (
-    <div>
-      <h1>Restaurant Form</h1>
-      <form>
-        <div className="form-group">
-          <label htmlFor="name-input">Name</label>
-          <input type="text" className="form-control" id="name-input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="address-input">Address</label>
-          <input type="text" className="form-control" id="address-input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="opening-time-input">Opening Time</label>
-          <input type="text" className="form-control" id="opening-time-input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="closing-time-input">Closing Time</label>
-          <input type="text" className="form-control" id="closing-time-input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="cuisine-input">Cuisine</label>
-          <input type="text" className="form-control" id="cuisine-input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="price-input">Average Price</label>
-          <input
-            type="number"
-            min="1"
-            className="form-control"
-            id="price-input"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="image-url-input">Image URL</label>
-          <input type="text" className="form-control" id="image-url-input" />
-        </div>
-        <button className="btn btn-primary btn-sm" onClick={handleClick}>
-          Save
-        </button>
-      </form>
-    </div>
-  );
+  render() {
+    const { cuisines } = this.state;
+    return (
+      <div>
+        <h1>Restaurant Form</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="name-input">Name</label>
+            <input type="text" className="form-control" id="name-input" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address-input">Address</label>
+            <input type="text" className="form-control" id="address-input" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="opening-time-input">Opening Time</label>
+            <input
+              type="text"
+              className="form-control"
+              id="opening-time-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="closing-time-input">Closing Time</label>
+            <input
+              type="text"
+              className="form-control"
+              id="closing-time-input"
+            />
+          </div>
+          <SelectInput name="cuisine" label="Cuisine" options={cuisines} />
+          <div className="form-group">
+            <label htmlFor="price-input">Average Price</label>
+            <input
+              type="number"
+              min="1"
+              className="form-control"
+              id="price-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="image-url-input">Image URL</label>
+            <input type="text" className="form-control" id="image-url-input" />
+          </div>
+          <button className="btn btn-primary btn-sm" onClick={this.handleClick}>
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default RestaurantForm;
