c2b7b6b92df1dd99a5f44bf03cb1f90091ba1c4b
edsonha <edsonha@gmail.com>
2020-01-15 14:23:45 +0800

    Add Form in restaurant form page

--- src/components/RestaurantForm/RestaurantForm.js
+++ src/components/RestaurantForm/RestaurantForm.js
@@ -8,9 +8,44 @@ function RestaurantForm(props) {
   return (
     <div>
       <h1>Restaurant Form</h1>
-      <button className="btn btn-primary btn-sm" onClick={handleClick}>
-        Save
-      </button>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name-input">Name</label>
+          <input type="text" className="form-control" id="name-input" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address-input">Address</label>
+          <input type="text" className="form-control" id="address-input" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="opening-time-input">Opening Time</label>
+          <input type="text" className="form-control" id="opening-time-input" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="closing-time-input">Closing Time</label>
+          <input type="text" className="form-control" id="closing-time-input" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="cuisine-input">Cuisine</label>
+          <input type="text" className="form-control" id="cuisine-input" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price-input">Average Price</label>
+          <input
+            type="number"
+            min="1"
+            className="form-control"
+            id="price-input"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="image-url-input">Image URL</label>
+          <input type="text" className="form-control" id="image-url-input" />
+        </div>
+        <button className="btn btn-primary btn-sm" onClick={handleClick}>
+          Save
+        </button>
+      </form>
     </div>
   );
 }
