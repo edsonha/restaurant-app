a5d60af69f8d9381f6182b4ccd74e4cd5ac3c5b8
edsonha <edsonha@gmail.com>
2020-01-15 15:06:46 +0800

    Extract form input into separate component to reduce repeating code

--- src/components/RestaurantForm/RestaurantForm.js
+++ src/components/RestaurantForm/RestaurantForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getCuisines } from "../../services/cuisineService";
+import Input from "../common/Input/Input";
 import SelectInput from "../common/Input/SelectInput";
 
 class RestaurantForm extends Component {
@@ -20,44 +21,13 @@ class RestaurantForm extends Component {
       <div>
         <h1>Restaurant Form</h1>
         <form>
-          <div className="form-group">
-            <label htmlFor="name-input">Name</label>
-            <input type="text" className="form-control" id="name-input" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="address-input">Address</label>
-            <input type="text" className="form-control" id="address-input" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="opening-time-input">Opening Time</label>
-            <input
-              type="text"
-              className="form-control"
-              id="opening-time-input"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="closing-time-input">Closing Time</label>
-            <input
-              type="text"
-              className="form-control"
-              id="closing-time-input"
-            />
-          </div>
+          <Input name="name" label="Name" />
+          <Input name="address" label="Address" />
+          <Input name="openingTime" label="Opening Time" />
+          <Input name="closingTime" label="Closing Time" />
           <SelectInput name="cuisine" label="Cuisine" options={cuisines} />
-          <div className="form-group">
-            <label htmlFor="price-input">Average Price</label>
-            <input
-              type="number"
-              min="1"
-              className="form-control"
-              id="price-input"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="image-url-input">Image URL</label>
-            <input type="text" className="form-control" id="image-url-input" />
-          </div>
+          <Input name="average-price" label="Average Price" type="number" />
+          <Input name="image-url" label="Image URL" />
           <button className="btn btn-primary btn-sm" onClick={this.handleClick}>
             Save
           </button>
