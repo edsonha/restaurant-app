a0b67f3f2c366c4f7686c651ed9c12afcaee42d5
edsonha <edsonha@gmail.com>
2020-01-16 22:05:34 +0800

    Add front end validation on resturant form using joi

--- src/components/RestaurantForm/RestaurantForm.js
+++ src/components/RestaurantForm/RestaurantForm.js
@@ -7,6 +7,7 @@ import {
 import Input from "../common/input/Input";
 import SelectInput from "../common/input/SelectInput";
 import TimeInput from "../common/input/TimeInput";
+import Joi from "joi-browser";
 
 class RestaurantForm extends Component {
   constructor(props) {
@@ -21,9 +22,26 @@ class RestaurantForm extends Component {
         cuisineId: "",
         averagePrice: "",
         imageUrl: ""
-      }
+      },
+      errors: {}
     };
   }
+
+  schema = {
+    _id: Joi.string(),
+    name: Joi.string().required(),
+    address: Joi.string().required(),
+    openingTime: Joi.string(),
+    closingTime: Joi.string(),
+    cuisineId: Joi.string().required(),
+    averagePrice: Joi.number()
+      .integer()
+      .min(1)
+      .required(),
+    imageUrl: Joi.string()
+      .uri({ allowRelative: true })
+      .required()
+  };
 
   componentDidMount() {
     const { id } = this.props.match.params;
@@ -40,6 +58,9 @@ class RestaurantForm extends Component {
 
     const { cuisineId, averagePrice } = this.state.data;
     const cuisine = getCuisines().find(cuisine => cuisine._id === cuisineId);
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
     const restaurant = { ...this.state.data };
     delete restaurant.cuisineId;
     restaurant.cuisine = cuisine;
@@ -49,14 +70,38 @@ class RestaurantForm extends Component {
     this.props.history.replace(this.props.returnPath);
   };
 
+  validateField = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  validate = () => {
+    const data = { ...this.state.data };
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(data, this.schema, options);
+    if (!error) return null;
+    const errors = error.details.reduce((acc, item) => {
+      acc[item.path[0]] = item.message;
+      return acc;
+    }, {});
+    return errors;
+  };
+
   handleChange = ({ target: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateField(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const data = { ...this.state.data };
     data[input.name] = input.value;
-    this.setState({ data });
+    this.setState({ data, errors });
   };
 
   render() {
-    const { cuisines } = this.state;
+    const { cuisines, errors } = this.state;
     const {
       name,
       address,
@@ -76,12 +121,14 @@ class RestaurantForm extends Component {
             label="Name"
             onChange={this.handleChange}
             value={name}
+            error={errors.name}
           />
           <Input
             name="address"
             label="Address"
             onChange={this.handleChange}
             value={address}
+            error={errors.address}
           />
           <TimeInput
             name="openingTime"
@@ -101,6 +148,7 @@ class RestaurantForm extends Component {
             options={cuisines}
             onChange={this.handleChange}
             value={cuisineId}
+            error={errors.cuisineId}
           />
           <Input
             name="averagePrice"
@@ -108,14 +156,18 @@ class RestaurantForm extends Component {
             type="number"
             onChange={this.handleChange}
             value={averagePrice}
+            error={errors.averagePrice}
           />
           <Input
             name="imageUrl"
             label="Image URL"
             onChange={this.handleChange}
             value={imageUrl}
+            error={errors.imageUrl}
           />
-          <button className="btn btn-primary btn-sm">Save</button>
+          <button disabled={this.validate()} className="btn btn-primary btn-sm">
+            Save
+          </button>
         </form>
       </div>
     );
