46b84fedb1841d11c7e79436d1903a910bfe3572
edsonha <edsonha@gmail.com>
2020-01-16 22:12:55 +0800

    Add restaurant form validation test

--- src/components/RestaurantForm/RestaurantForm.test.js
+++ src/components/RestaurantForm/RestaurantForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import RestaurantForm from "./RestaurantForm";
 
 describe("Restaurant Form", () => {
@@ -14,5 +14,42 @@ describe("Restaurant Form", () => {
     expect(getByLabelText("Cuisine")).toBeInTheDocument();
     expect(getByLabelText("Average Price")).toHaveAttribute("type", "number");
     expect(getByLabelText("Image URL")).toHaveAttribute("type", "text");
+  });
+
+  it("should have disabled save button on page load", () => {
+    const match = { params: { id: 1 } };
+    const { getByText } = render(<RestaurantForm match={match} />);
+    expect(getByText("Save")).toHaveAttribute("disabled");
+  });
+
+  it("should display error message if text input is invalid", () => {
+    const match = { params: { id: 1 } };
+    const message = /is not allowed to be empty/i;
+    const { getByText, queryByText, getByLabelText } = render(
+      <RestaurantForm match={match} />
+    );
+    const input = getByLabelText(/name/i);
+
+    fireEvent.change(input, { target: { value: "a" } });
+    fireEvent.change(input, { target: { value: "" } });
+    expect(getByText(message)).toBeInTheDocument();
+
+    fireEvent.change(input, { target: { value: "Restaurant 1" } });
+    expect(queryByText(message)).not.toBeInTheDocument();
+  });
+
+  it("should display error message if number input is invalid", () => {
+    const match = { params: { id: 1 } };
+    const message = /must be larger than or equal to 1/i;
+    const { getByText, queryByText, getByLabelText } = render(
+      <RestaurantForm match={match} />
+    );
+    const input = getByLabelText(/Average Price/i);
+
+    fireEvent.change(input, { target: { value: "0" } });
+    expect(getByText(message)).toBeInTheDocument();
+
+    fireEvent.change(input, { target: { value: "1" } });
+    expect(queryByText(message)).not.toBeInTheDocument();
   });
 });
