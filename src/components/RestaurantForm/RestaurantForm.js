37e5b09eae28cc5e7cfe7c4d20f3ad0f8ee30461
edsonha <edsonha@gmail.com>
2020-01-16 13:38:10 +0800

    Modify restaurant form title to display Edit Restaurant when edit is clicked

--- src/components/RestaurantForm/RestaurantForm.js
+++ src/components/RestaurantForm/RestaurantForm.js
@@ -66,9 +66,10 @@ class RestaurantForm extends Component {
       averagePrice,
       imageUrl
     } = this.state.data;
+    const restaurantId = this.props.match.params.id;
     return (
       <div data-testid="create-page">
-        <h3>New Restaurant</h3>
+        <h3>{restaurantId ? "Edit Restaurant" : "New Restaurant"}</h3>
         <form onSubmit={this.handleSubmit}>
           <Input
             name="name"
